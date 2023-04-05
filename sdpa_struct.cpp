a1bf92e9f01c5a4710662173339a1340a3c773b0
NAKATA Maho <maho.nakata@gmail.com>
2023-04-05 11:14:04 +0900

    also fixes #1

--- sdpa_struct.cpp
+++ sdpa_struct.cpp
@@ -771,9 +771,11 @@ void DenseMatrix::display(FILE* fpout)
       fprintf(fpout,"  {");
     }
     for (int j=0; j<nCol-1; ++j) {
-      fprintf(fpout,P_FORMAT",",(double)de_ele[(nRow-1)+nCol*j]);
+      quadmath_snprintf(mpbuffer_sdpa_struct,BINARY128BUFFER,PQ_FORMAT,de_ele[(nRow-1)+nCol*j]); //issue #1
+      fprintf(fpout,"%s,",mpbuffer_sdpa_struct); //issue #1
     }
-    fprintf(fpout,P_FORMAT" }",(double)de_ele[(nRow-1)+nCol*(nCol-1)]);
+    quadmath_snprintf(mpbuffer_sdpa_struct,BINARY128BUFFER,PQ_FORMAT,de_ele[(nRow-1)+nCol*(nCol-1)]); //issue #1
+    fprintf(fpout,"%s }",mpbuffer_sdpa_struct); //issue #1
     if (nRow>1) {
       fprintf(fpout,"   }\n");
     } else {
